07dd25eb9afab96d9311f2e4c5b3854b8b28ce4d
Aracely Hernández Hernández <aracely.hernandez@MacBook-Air-de-Mac.local>
2025-03-14 17:34:30 -0600

    Agregué mas cosas para el login

--- front/src/components/usuario/login.tsx
+++ front/src/components/usuario/login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Form, Container, Row, Col, InputGroup } from "react-bootstrap";
-import '../../css/login.css'; 
+import "../../css/login.css";
+import loginImage from "../../assets/login.jpg";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
@@ -18,51 +19,59 @@ export const Login = () => {
   };
 
   return (
-    <Container className="login-container">
-      <h2>Iniciar sesión</h2>
+    <>
+      <div className="login">
+        <div className="login-box">
+        <div className="illustration-wrapper">
+        <img src={loginImage} alt="Login" />
+        </div>
+          <Container>
+            <h2>Iniciar sesión</h2>
 
-      {error && <div className="alert alert-danger">{error}</div>}
+            {error && <div className="alert alert-danger">{error}</div>}
+            <Form className="container-form" onSubmit={handleSubmit}>
+              <Form.Group as={Row} controlId="formEmail" className="mb-3">
+                <Col sm="12">
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <i className="bi bi-person-fill"></i>
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="email"
+                      placeholder="Usuario"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </InputGroup>
+                </Col>
+              </Form.Group>
 
-      <Form onSubmit={handleSubmit}>
-        <Form.Group as={Row} controlId="formEmail" className="mb-3">
-          <Col sm="12">
-            <InputGroup>
-              <InputGroup.Text>
-                <i className="bi bi-person-fill"></i>
-              </InputGroup.Text>
-              <Form.Control
-                type="email"
-                placeholder="Usuario"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </InputGroup>
-          </Col>
-        </Form.Group>
+              <Form.Group as={Row} controlId="formPassword" className="mb-4">
+                <Col sm="12">
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <i className="bi bi-lock-fill"></i>
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="password"
+                      placeholder="Contraseña"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <InputGroup.Text>
+                      <i className="bi bi-eye-slash-fill"></i>
+                    </InputGroup.Text>
+                  </InputGroup>
+                </Col>
+              </Form.Group>
 
-        <Form.Group as={Row} controlId="formPassword" className="mb-4">
-          <Col sm="12">
-            <InputGroup>
-              <InputGroup.Text>
-                <i className="bi bi-lock-fill"></i>
-              </InputGroup.Text>
-              <Form.Control
-                type="password"
-                placeholder="Contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <InputGroup.Text>
-                <i className="bi bi-eye-slash-fill"></i>
-              </InputGroup.Text>
-            </InputGroup>
-          </Col>
-        </Form.Group>
-
-        <Button variant="primary" type="submit" className="w-100">
-          Iniciar sesión
-        </Button>
-      </Form>
-    </Container>
+              <Button variant="primary" type="submit" className="w-100">
+                Iniciar sesión
+              </Button>
+            </Form>
+          </Container>
+        </div>
+      </div>
+    </>
   );
 };
