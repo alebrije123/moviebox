97d1e09d92e8da1721df81c1725f244a10143dfa
Aracely Hernández Hernández <aracely.hernandez@MacBook-Air-de-Mac.local>
2025-03-18 17:21:39 -0600

    footer y navbar avance

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+
 import "./App.css";
 import { Login } from "./components/usuario/login.tsx";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Home } from "./components/home/home.tsx";
+import { MyNavbar } from "./components/layouts/navbar.tsx";
+import { Footer } from "./components/layouts/footer.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <MyNavbar />
+      <div className="content">
+        <Home />
+      </div>
+      <Footer />
     </div>
   );
 }
